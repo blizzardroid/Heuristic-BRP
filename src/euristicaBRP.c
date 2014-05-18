755587f2487295c546b7c460618ed41a5513dd58
Andrea Mancini <andrea.mancini14@studio.unibo.it>
2014-05-18 21:07:39 +0200

    Versione 1.0

--- src/euristicaBRP.c
+++ src/euristicaBRP.c
@@ -65,11 +65,11 @@ FILE * file;
 
 
 printf("------Euristica Min-Max del Blocks Relocation Problem (BRP)------\n");
-printf("------    Università di Bologna    ------\n");
+printf("-----------------      Università di Bologna    -------------\n");
 
 file= fopen ("//home//blizzard//Scrivania//BRP//istanze//data3-5-34.dat","r");
 if (file!=NULL){
-	printf("Aperto file dati \n");
+	printf("Aperto file dati dell'istanza iniziale.... \n");
 }
 
 fscanf(file, "%d", &NSTACK);
@@ -94,7 +94,7 @@ for(i=0;i<=NSTACK-1; i++){
 }
 
 //Applicazione dell'algoritmo sull'istanza bay per  NBLOCK-1
-for(priority=1;priority<=3; priority++){
+for(priority=1;priority<=NBLOCK-1; priority++){
 	//da far iterare la parte successiva
 
 	//Troviamo l'insieme R e la sua dimensione
@@ -150,6 +150,8 @@ for(priority=1;priority<=3; priority++){
 		for(s_star=0;s_star<=NSTACK-1;s_star++){
 			if(min_sort[s_star]>r){
 				//abbiamo trovato il più piccolo valore di min[i] che sia maggiore di r
+				printf("Trovato: ");
+				printf(" min[i]=%d>%d ",min_sort[s_star],r);
 				for(i=0;i<=NSTACK-1;i++){
 				  for(j=0;j<=(NSTACK-1)*3;j++){
 					  if(bay[i][j]==r){
@@ -162,10 +164,12 @@ for(priority=1;priority<=3; priority++){
 					for(j=0;j<=(NSTACK-1)*3;j++){
 						if(bay[i][j]==min_sort[s_star]){
 							s_star=i;
-							printf("blocco %d da rilocare in posizione %d di min[i] \n",r,s_star);
+							printf("\nBlocco %d da rilocare in posizione %d di min[i] \n",r,s_star+1);
+							break;
 						}
 					}
 				}
+				printf("\n");
 				//ora s_star contiene lo stack da rilocare
 				for(i=0;i<=NSTACK-1;i++){
 					if(bay[s_star][i]==0){
@@ -174,6 +178,7 @@ for(priority=1;priority<=3; priority++){
 					}
 				}
 			flag=1;
+			break;// non considero gli altri min[i]
 			}
 		}
 
@@ -182,7 +187,7 @@ for(priority=1;priority<=3; priority++){
 		//non siamo riusciti a trovare in min[i] nessun valore > r
 		if(flag==0){//altrimenti lo rilochiamo nel max{min[i]}
 			maxstack=find_max_stack(min,NSTACK-1);
-			printf("DEBUG: maxstack=%d \n",maxstack);
+			printf("Non ho trovato nessun min[i]>%d considero maxstack=%d \n",r,maxstack+1);
 			for(i=0;i<=NSTACK-1;i++){
 			  for(j=0;j<=NSTACK-1;j++){
 				  if(bay[i][j]==r){
@@ -212,19 +217,21 @@ for(priority=1;priority<=3; priority++){
 	}//while(dimR!=0)
 
 	//ora posso togliere il blocco dalla baia
+	printf("\ntolgo il blocco %d dalla baia \n\n",priority);
 	for(i=0;i<=NSTACK-1;i++){
 		for(j=0;j<=(NSTACK-1)*3;j++){
 			if(bay[i][j]==priority){
 				bay[i][j]=0; //togliamo il blocco dalla baia
-				printf("tolgo il blocco %d dalla baia \n",bay[i][j]);
 			}
 			printf("%d ",bay[i][j]);
 		}
 		printf("\n");
 	}
+	printf("\n------------\n");
 
 }//for(priority=1;....
 
+printf("tolgo il blocco finale %d dalla baia \n",priority);
 fclose(file);
 return(0);
 }
